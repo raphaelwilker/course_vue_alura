a8e78a563bb3a1dcd0db01b10bd4d92ec8617cb9
raphaelwilker <raphaelwilkermendes@gmail.com>
2019-04-28 22:04:26 -0300

    master: component/shared/

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,25 +1,31 @@
 <template>
-  <div>
-    <h1 v-text="titulo"></h1>
+  <div class="corpo">
+    <h1 class="titulo centralizado" v-text="titulo"></h1>
     <!-- 
       : antes do attributo é igual a v-bind.
       O vue trabalha com o mesmo sitema que o angular tem no ng-bind
     -->
     <!-- <img :src="photo.url" :alt="photo.alt"/> -->
-    <ul>
+    <ul class="lista-photos">
 
-        <li v-for="foto in fotos">
-            <img :src="foto.url" :alt="foto.titulo">
-        </li>
-
-        
+      <li class="lista-photos-item" v-for="foto in fotos">
+        <meu-painel :titulo="foto.titulo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+      </li>
 
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
 export default {
+    
+  components: {
+    'meu-painel': Painel
+  },
+  
   data(){
     return{
       titulo: 'Alura pic',
@@ -37,4 +43,6 @@ export default {
 </script>
 
 <style>
+  /* @import url(app.css); */
+  
 </style>
---
+++ alurapic/src/components/shared/painel/Painel.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div class="painel">
+        <h2 class="painel-titulo">{{titulo}}</h2>
+        <slot class="painel-corpo">
+        </slot><!-- fim painel-corpo -->
+    </div><!-- fim painel -->
+
+
+</template>
+
+<script>
+export default {
+
+  props: ['titulo']
+
+}
+</script>
