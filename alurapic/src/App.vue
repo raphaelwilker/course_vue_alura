ddc057ddfa7e4a1e56eee7aab7f91091d95c220c
Raphael Araujo/LGESP CNS - SSP(raphael.araujo@lgepartner.com) <raphael.araujo@lge.com>
2019-05-06 17:51:14 -0300

    master: SPA

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,66 +1,49 @@
 <template>
   <div class="corpo">
-    <h1 class="titulo centralizado" v-text="titulo"></h1>
 
-    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
-    {{filtro}}
-    <!-- 
-      : antes do attributo é igual a v-bind.
-      O vue trabalha com o mesmo sitema que o angular tem no ng-bind
-    -->
-    <!-- <img :src="photo.url" :alt="photo.alt"/> -->
-    <ul class="lista-photos">
+    <meu-menu :rotas="routes"/>
 
-      <li class="lista-photos-item" v-for="foto of fotosComFiltro">
-        <meu-painel :titulo="foto.titulo">
-            <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-        </meu-painel>
-      </li>
-
-    </ul>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel.vue';
-import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
-
-export default {
-    
-  components: {
-    'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva
-  },
   
-  data(){
-    return{
-      titulo: 'Alura pic',
-      fotos: [],
-      filtro: ''
-    }
-  },
+  import { routes } from './routes';
+  import Menu from './components/shared/menu/Menu.vue';
 
-  computed: {
-    fotosComFiltro() {
-      if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
+  export default {
+
+    components: {
+      'meu-menu' : Menu
+    },
+
+    data(){
+
+      return {
+
+        routes
       }
-    }
-  },
 
-  created(){
-    let promise = this.$http.get('http://localhost:3000/v1/fotos');
-    promise
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos);
+    }
+
   }
-}
+
 </script>
 
 <style>
-  /* @import url(app.css); */
-  
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .3s
+  }
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
+  }
 </style>
---
+++ alurapic/src/components/home/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div >
+    <h1 class="titulo centralizado" v-text="titulo"></h1>
+
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
+    {{filtro}}
+    <!-- 
+      : antes do attributo é igual a v-bind.
+      O vue trabalha com o mesmo sitema que o angular tem no ng-bind
+    -->
+    <!-- <img :src="photo.url" :alt="photo.alt"/> -->
+    <ul class="lista-photos">
+
+      <li class="lista-photos-item" v-for="foto of fotosComFiltro">
+        <meu-painel :titulo="foto.titulo">
+            <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+        </meu-painel>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+import Painel from '../shared/painel/Painel.vue';
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+
+export default {
+    
+  components: {
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva
+  },
+  
+  data(){
+    return{
+      titulo: 'Alura pic',
+      fotos: [],
+      filtro: ''
+    }
+  },
+
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
+  },
+
+  created(){
+    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+    promise
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos);
+  }
+}
+</script>
+
+<style>
+  /* @import url(app.css); */
+  
+</style>
